24d2f4b2f529275135e456b408b5b8003caf0aa1
Olivier Cervello <ocervello@google.com>
2021-09-29 12:57:42 +0200

    Fix install docker script

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -14,19 +14,11 @@ wget https://github.com/containers/fuse-overlayfs/releases/download/v1.7.1/fuse-
 chmod +x fuse-overlayfs-x86_64
 sudo mv fuse-overlayfs-x86_64 /usr/local/bin/fuse-overlayfs
 
-# Modify Docker storage driver
-echo '{ "storage-driver": "overlay" }' | sudo tee /etc/docker/daemon.json > /dev/null
+# Add user to Docker group if supplied as input
+[ -z "$1" ] && sudo usermod -g docker $1
 
 # Restart Docker host
 sudo systemctl restart docker
-
-# Add user to Docker group if supplied as input
-if [ -z "$1" ]
-then
-    echo "No user supplied. Will not add user to Docker group."
-else
-    sudo usermod -g docker $1
-fi
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
