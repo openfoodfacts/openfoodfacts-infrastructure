5af9e8a74f1684c0edc1dea0ac2e9207faab6b08
Charles Nepote <charles@nepote.org>
2022-09-27 13:07:29 +0200

    Minor improvements

--- scripts/mirabelle/products_daily_updates.sh
+++ scripts/mirabelle/products_daily_updates.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 path=/home/off/mirabelle
 cd $path
+mode="" # Enter "i" to get the interactive mode
 
 export PATH="/usr/local/bin:$PATH"
 rm en.openfoodfacts.org.products.csv.bak products.db.bak products_new.db
 old_csv=$(wc -c en.openfoodfacts.org.products.csv | awk '{print $1}')
 old_csv_lines=$(wc -l en.openfoodfacts.org.products.csv | awk '{print $1}')
 echo "Old CSV weights $old_csv bytes for $old_csv_lines lines"
-read -p "Press [Enter] key to downloaf CSV..."
+[[$mode = "i"]] && read -p "Press [Enter] key to downloaf CSV..."
 
 wget -c https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv -O newdata.csv
 new_csv=$(wc -c newdata.csv | awk '{print $1}')
@@ -16,7 +17,7 @@ echo "New CSV weights $new_csv bytes for $new_csv_lines lines"
 
 if [ "$new_csv" -ge "$old_csv" ]
 then
-  read -p "Press [Enter] key to create new db..."
+  [[$mode = "i"]] && read -p "Press [Enter] key to create new db..."
   mv en.openfoodfacts.org.products.csv en.openfoodfacts.org.products.csv.bak
   mv newdata.csv en.openfoodfacts.org.products.csv
   echo `date`;
@@ -38,9 +39,10 @@ EOS
 # real    2m2.761s
 else
   echo "CSV error?"
+  exit 1
 fi
 
-#read -p "Press [Enter] key to continue..."
+[[$mode = "i"]] && read -p "Press [Enter] key to continue..."
 rm en.openfoodfacts.org.products.csv.bak
 
 # Converting empty to NULL for columns which are either FLOAT or INTEGER
