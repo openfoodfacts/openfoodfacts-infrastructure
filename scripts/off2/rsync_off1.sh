735e1ba6906c5b84f8bf73abd64513977dacb9b9
Alex Garel <alex@garel.org>
2023-02-13 10:53:11 +0100

    wip on rsync_off1.sh

--- scripts/off2/rsync_off1.sh
+++ scripts/off2/rsync_off1.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-nice -n 19 ionice -c 3 rsync -a -x -e "ssh -T -o Compression=no -x -i /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv/ /srv/ --exclude '*.old' &
-nice -n 19 ionice -c 3 rsync -a -x -e "ssh -T -o Compression=no -x -i /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv2/ /srv2/ &
+# run this script as root
+
+nice -n 19 ionice -c 3 \
+  rsync -a -x -e "ssh -T -o Compression=no -x -i \
+  --exclude="lost+found" \
+  /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv/ /srv/ --exclude '*.old' &
+nice -n 19 ionice -c 3 \
+  rsync -a -x -e "ssh -T -o Compression=no -x -i \
+  /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv2/ /srv2/ &
 wait
 
 # copy off photos to ovh3
+# Currently is done manually through
 
 # we prefer to split work
 # use list files and let xargs divide work
-ls /srv2/off/html/images/products/ | \
-xargs -IDIRECTORIES \
-  rsync -a -x -e "ssh -T -x -i /home/off/.ssh/off2_rsync_id_rsa" DIRECTORIES raphael0202@ovh3.openfoodfacts.org:/rpool/off/images/products/
+#ls /srv2/off/html/images/products/ | \
+#xargs -IDIRECTORIES \
+#  rsync -a -x -e "ssh -T -x -i /home/off/.ssh/off2_rsync_id_rsa" DIRECTORIES raphael0202@ovh3.openfoodfacts.org:/rpool/off/images/products/
+
+
+find . ! \( -user "$u" -perm -u=r -o \
+          ! -user "$u" \( -group $g \) -perm -g=r -o \
+          ! -user "$u" ! \( -group $g \) -perm -o=r \)
