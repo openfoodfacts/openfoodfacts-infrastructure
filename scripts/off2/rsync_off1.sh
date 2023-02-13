63eb5d65d65f0f0ff54c48bbaf250eecea6462f6
Alex Garel <alex@garel.org>
2023-02-13 18:01:50 +0100

    fix: rsync only off data

--- scripts/off2/rsync_off1.sh
+++ scripts/off2/rsync_off1.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-# run this script as root
+# run this script as root on off2 side to avoid permission changes problems
 
+# sync of /srv
 nice -n 19 ionice -c 3 \
   rsync -a -x -e "ssh -T -o Compression=no -x -i \
   --exclude="lost+found" \
   /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv/ /srv/ --exclude '*.old' &
-nice -n 19 ionice -c 3 \
-  rsync -a -x -e "ssh -T -o Compression=no -x -i \
-  /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv2/ /srv2/ &
+# sync of /srv2
+for dir_name in off off-pro
+do
+  nice -n 19 ionice -c 3 \
+    rsync -a -x -e "ssh -T -o Compression=no -x -i \
+    /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv2/$dir_name /srv2/$dir_name &
+done
+
 wait
 
 # copy off photos to ovh3
-# Currently is done manually through
-
-# we prefer to split work
-# use list files and let xargs divide work
-#ls /srv2/off/html/images/products/ | \
-#xargs -IDIRECTORIES \
-#  rsync -a -x -e "ssh -T -x -i /home/off/.ssh/off2_rsync_id_rsa" DIRECTORIES raphael0202@ovh3.openfoodfacts.org:/rpool/off/images/products/
-
-
-find . ! \( -user "$u" -perm -u=r -o \
-          ! -user "$u" \( -group $g \) -perm -g=r -o \
-          ! -user "$u" ! \( -group $g \) -perm -o=r \)
+# Currently this is done manually through sync2ovh3.sh
