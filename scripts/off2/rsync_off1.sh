ce002d90a35bf21c10ae6dd86b3452ef81d99fae
Alex Garel <alex@garel.org>
2023-02-13 18:56:29 +0100

    fix: fix rsync_off1.sh

--- scripts/off2/rsync_off1.sh
+++ scripts/off2/rsync_off1.sh
@@ -4,15 +4,15 @@
 
 # sync of /srv
 nice -n 19 ionice -c 3 \
-  rsync -a -x -e "ssh -T -o Compression=no -x -i \
-  --exclude="lost+found" \
-  /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv/ /srv/ --exclude '*.old' &
+  rsync -a -x -e "ssh -T -o Compression=no -x -i /root/.ssh/off2_rsync_id_rsa" \
+  --exclude="lost+found" --exclude '*.old' \
+  off@10.0.0.1:/srv/ /srv/  &
 # sync of /srv2
 for dir_name in off off-pro
 do
   nice -n 19 ionice -c 3 \
-    rsync -a -x -e "ssh -T -o Compression=no -x -i \
-    /home/off/.ssh/off2_rsync_id_rsa" off@10.0.0.1:/srv2/$dir_name /srv2/$dir_name &
+    rsync -a -x -e "ssh -T -o Compression=no -x -i /root/.ssh/off2_rsync_id_rsa" \
+    off@10.0.0.1:/srv2/$dir_name /srv2/$dir_name &
 done
 
 wait
