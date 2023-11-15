232d202e68e1ecf288ebd2321767ccd36a466a3d
Alex Garel <alex@openfoodfacts.org>
2023-11-15 17:30:56 +0000

    feat: better clones update script still not working totally as expected

--- scripts/ovh3/maj-clones-nfs-VM-dockers.sh
+++ scripts/ovh3/maj-clones-nfs-VM-dockers.sh
@@ -2,25 +2,32 @@
 
 # Script de remise à jour des clones (R/W) des données off (users/orgs/images/products)
 
-# stop staging environment as it uses clones
-ssh off@10.1.0.200 -J root@10.0.0.1 'cd /home/off/off-net/ && docker-compose stop'
+SNAP=$(date +%Y%m%d%H%M%S)
 
-# restart nfs because sometimes it otherwise creates problems
-systemctl restart nfs-server.service
+# stop docker for openfoodfacts-net (staging)
+# ssh not working yet, but I don't know why :'(
+echo ssh 10.1.0.200 sudo -u off bash -c "cd /home/off/off-net;docker-compose stop"
+read -p "waiting for you to do it"
 
-# update clones
+# Data synced with sanoid / syncoid
 for DATA in orgs users images
 do
-	# we use last daily snapshot
-	SNAP=$(zfs list -t snap -o name -s creation rpool/off/$DATA |grep 'autosnap_.*_daily'|tail -n 1)
+	echo regenerating clone for $DATA
+	LAST=$(zfs list -t snap rpool/off/$DATA -o name | grep '_daily$' | tail -n 1)
 	zfs destroy rpool/off/clones/$DATA
-	zfs clone $SNAP rpool/off/clones/$DATA
+	zfs clone $LAST rpool/off/clones/$DATA
 done
 
 # dernier snapshot quotidien pour "products" qui est en sync ZFS
+echo regenerating clone for products
 LAST=$(zfs list -t snap rpool/off/products -o name | grep '0000$' | tail -n 1)
 zfs destroy rpool/off/clones/products
 zfs clone $LAST rpool/off/clones/products
 
-# restart staging environment
-ssh off@10.1.0.200 -J root@10.0.0.1 'cd /home/off/off-net/ && docker-compose up -d'
+# reboot de la VM "dockers" pour remonter les volumes NFS
+# ssh 10.1.0.200 reboot
+
+# restart docker for openfoodfacts-net (staging)
+# ssh not working yet, but I don't know why :'(
+echo ssh 10.1.0.200 sudo -u off bash -c "cd /home/off/off-net;docker-compose start"
+read -p "waiting for you to do it"
