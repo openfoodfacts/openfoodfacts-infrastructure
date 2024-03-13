e1bc4ab5668efff34e4a7b7f6c1a622f5d34d91c
Alex Garel <alex@openfoodfacts.org>
2024-03-13 16:59:01 +0000

    fix: maj clones update

--- scripts/ovh3/maj-clones-nfs-VM-dockers.sh
+++ scripts/ovh3/maj-clones-nfs-VM-dockers.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 # Script de remise à jour des clones (R/W) des données off (users/orgs/images/products)
 
@@ -10,19 +10,13 @@ echo ssh 10.1.0.200 sudo -u off bash -c "cd /home/off/off-net;docker-compose sto
 read -p "waiting for you to do it"
 
 # Data synced with sanoid / syncoid
-for DATA in orgs users images
+for DATA in orgs users images products
 do
 	echo regenerating clone for $DATA
 	LAST=$(zfs list -t snap rpool/off/$DATA -o name | grep '_daily$' | tail -n 1)
 	zfs destroy rpool/off/clones/$DATA
 	zfs clone $LAST rpool/off/clones/$DATA
 done
-
-# dernier snapshot quotidien pour "products" qui est en sync ZFS
-echo regenerating clone for products
-LAST=$(zfs list -t snap rpool/off/products -o name | grep '0000$' | tail -n 1)
-zfs destroy rpool/off/clones/products
-zfs clone $LAST rpool/off/clones/products
 
 # reboot de la VM "dockers" pour remonter les volumes NFS
 # ssh 10.1.0.200 reboot
