f2c55371ab54ceca0bfc9231ef7ad970a9565f3c
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2025-05-03 00:37:01 +0200

    feat: adding certbot

---
+++ ansible/roles/reverse-proxy/templates/certbot/certbot-renew.sh
@@ -0,0 +1,14 @@
+# {{ ansible_managed }}
+
+certbot certonly \
+    --test-cert \
+    --non-interactive \
+    --agree-tos \
+    --renew-with-new-domains \
+    --cert-name "certificate" \
+    --preferred-challenges dns-01 \
+    --dns-ovh \
+    --dns-ovh-credentials /config/ovh.ini \
+    -m "{{ reverse_proxy_https_email }}" \
+    -d "{{ reverse_proxy_https_domain }}" \
+    -d "*.{{ reverse_proxy_https_domain }}"
