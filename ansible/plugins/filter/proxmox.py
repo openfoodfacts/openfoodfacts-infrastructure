b483390bca958af89af19761e465a25aa1c75f8c
Alex Garel <alex@garel.org>
2026-03-18 18:46:08 +0100

    feat: wip on opff move to scaleway

--- ansible/plugins/filter/proxmox.py
+++ ansible/plugins/filter/proxmox.py
@@ -10,6 +10,7 @@ class FilterModule:
             'proxmox_netif_dict': self.proxmox_netif_dict,
             'proxmox_netif_data_to_str': self.proxmox_netif_data_to_str,
             'proxmox_process_disk_volume': self.proxmox_process_disk_volume,
+            'proxmox_subuid_compare': self.proxmox_subuid_compare,
         }
 
     def proxmox_netif_dict(self, data, current_config):
@@ -79,3 +80,74 @@ class FilterModule:
             if storage == data.get("storage"):
                 result["volume"] = volume
         return result
+
+    def proxmox_subuid_compare(self, data, id_type, subuid_content, user="root"):
+        """Compare lxc.idmap settings to /etc/subuid or subgid
+
+        :param data: list of idmap settings, like "u 0 100000 999"
+        :param id_type: 'u' or 'g' if we care about user or gourp subuid
+        :param subuid_content: the content of /etc/subuid or /etc/subgid
+
+        :return: a list of lines that should be added to the file
+        """
+        if id_type not in ["u", "g"]:
+            raise AnsibleFilterError("id_type must be 'u' or 'g'")
+        expected_entries = []
+        for idmap in data:
+            idmap_type, _,start_id, num_ids = idmap.strip().split()
+            if idmap_type == id_type:
+                expected_entries.append((int(start_id), int(start_id) + int(num_ids)))
+        existing_entries = [l.split(":")[1:] for l in subuid_content.split("\n") if l.startswith(user + ":")]
+        existing_entries = [(int(start_id), int(start_id) + int(num_ids)) for start_id, num_ids in existing_entries]
+        existing_entries.sort()
+        entries_to_add = []
+        for start_id, end_id in expected_entries:
+            # get the existing entry overlapping
+            corresponding = [
+                (candidate_start_id, candidate_end_id)
+                for candidate_start_id, candidate_end_id in existing_entries
+                if (
+                    (candidate_start_id <= start_id and candidate_end_id >= start_id)
+                    or (candidate_start_id <= end_id and candidate_end_id >= end_id)
+                )
+            ]
+            if not corresponding:
+                entries_to_add.append((start_id, end_id))
+            if corresponding:
+                # check we don't have holes
+                # note: corresponding is already sorted
+                range_start, range_end = corresponding[0]
+                for candidate_start_id, candidate_end_id in corresponding[1:]:
+                    if candidate_start_id > range_end + 1:
+                        entries_to_add.append((range_end + 1, candidate_start_id - 1))
+                    range_end = max(range_end, candidate_end_id)
+                if range_end < end_id + 1:
+                    entries_to_add.append((range_end + 1, end_id))
+        entries_to_add.sort()
+        # we could remove or merge entries, won't do it for now as it's not really probable
+        lines_to_add = [
+            f"{user}:{start_id}:{end_id - start_id}"
+            for start_id, end_id in entries_to_add
+        ]
+        return lines_to_add
+
+if __name__ == "__main__":
+    # test
+    fmod = FilterModule()
+
+    result = fmod.proxmox_subuid_compare(
+        ["u 10 10 10", "g 10 10 10", "u 20 100 100"],
+        "u",
+        "root:100000:65536\nalex:165536:65536\n",
+    )
+    assert result == (
+        ["root:10:10", "root:100:100"]
+    )
+    result = fmod.proxmox_subuid_compare(
+        ["u 10 10 10", "g 10 10 10", "u 20 100 100"],
+        "u",
+        "root:0:30\nroot:100000:65536\nalex:165536:65536\n",
+    )
+    assert result == (
+        ["root:100:100"]
+    )
